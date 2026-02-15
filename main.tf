ccef58cf60d43984f4fb899fd41384ea51c38303
Yeruva Srinivas Reddy <66551639+srinu-1@users.noreply.github.com>
2026-02-15 19:17:00 +0530

    Fix formatting of ami line in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "server" {
   count         = var.instance_count
-  ami           = "ami-0f5ee92e2d63afc18" # Update for your region
+  ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = var.instance_type
 
   tags = {
